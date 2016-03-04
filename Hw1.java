c7f040a3f8b255ca6fdcf977c49a31c9233c85e2
queenstyle <102316230@gms.tcu.edu.tw>
2016-03-04 23:27:26 +0800

    hw1

---
+++ Hw1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hw1;
+
+/**
+ *
+ * @author fsa
+ */
+public class Hw1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("一為遷客去長沙");
+        System.out.println("西望長安不見家");
+        System.out.println("黃鶴樓中吹玉笛");
+        System.out.println("江城五月落梅花");
+    }
+
+}
